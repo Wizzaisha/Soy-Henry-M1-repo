3775b2dfaed802798fad88ce0a92483d7192ec2f
Wizzaisha <ever.ramirez@usantotomas.edu.co>
2022-05-23 12:04:12 -0500

    CS homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,41 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let bin = [...num];
+
+  let numDecimal = 0;
+
+  //(bit * 2 ^ index)
+
+  let index = 0;
+
+  for (let position = bin.length - 1; position >= 0; position--) {
+
+    numDecimal += (bin[index] * 2) ** position;
+
+    index++;
+
+  }
+
+  return numDecimal;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let nbin = [];
 
+  // dividir el numero x2 las veces que sean necesarias y leer de abajo hacia arriba
+
+  while (num > 1) {
+
+    nbin.unshift(Math.floor(num % 2));
+
+    num /= 2;
+
+  }
+
+  return nbin.join("");
 }
 
 
